a0fbae2717d9f4e7a273eeea2c1ef70362df8c66
Erick <erick.personal16@outlook.com>
2024-11-12 22:07:55 -0300

    soy Erick

--- ProyectoIntegrador/src/Hospital/gestion/Principal.java
+++ ProyectoIntegrador/src/Hospital/gestion/Principal.java
@@ -2,9 +2,10 @@ package Hospital.gestion;
 
 public class Principal {
 
-	public static void main(String[] args) {
-           //Soy Maxi
-          //quien soy? 
-	}
+    public static void main(String[] args) {
+        //Soy Maxi
+        //quien soy? 
+        // soy Erick
+    }
 
 }
