a6aa0ff0c4654d1f3f4ad12be99fe857dcf95baa
Erick <erick.personal16@outlook.com>
2024-11-15 20:47:30 -0300

    feat: Se agrego impresion de lista de medicos disponibles

--- ProyectoIntegrador/src/Hospital/gestion/GestionCirugias.java
+++ ProyectoIntegrador/src/Hospital/gestion/GestionCirugias.java
@@ -20,4 +20,23 @@ public class GestionCirugias {
             System.out.println("Se ha concretado la cirugia: " + cirugiaHecha);
         }
     }
+
+    public static void GestionConsultas() {
+        int opcion;
+        do {
+            opcion = Helper.validarEnteroEnRango(Principal.input, "Elije una opcion: ", 1, 9);
+            System.out.println(Helper.repetirLetra("-", 50));
+            ejecutarOpcion(opcion);
+            System.out.println(Helper.repetirLetra("_", 50));
+        } while (opcion != 3);
+    }
+
+    private static void ejecutarOpcion(int opcion) {
+        switch (opcion) {
+            case 1 -> {
+                System.out.println("Lista de medicos disponibles en el hospital:");
+                Principal.medicosDisponibles.InOrder();
+            }
+        }
+    }
 }
